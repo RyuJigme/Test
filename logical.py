8a252307fd4109a278d2b482aec42bcb0cb1361c
unknown <02230065.cst@rub.edu.bt>
2024-03-01 16:33:20 +0530

    Adding logical.py file

---
+++ logical.py
@@ -0,0 +1,24 @@
+#and operator
+# example 1
+x = 5
+print(x > 3 and x < 10)#True because both the conditions are true
+
+# example 2 
+y = 12
+print(y > 10 and y % 5 == 0) # False because the second condition is false 
+
+# or operator
+x = 5
+print(x > 3 or x < 10)#True because both the conditions are true
+
+# example 2 
+y = 12
+print(y > 10 or y % 5 == 0) # False because the second condition is false 
+
+#not operator
+x = 5
+print(not(x > 3 and x < 10))#True because both the conditions are true
+
+# example 2 
+y = 12
+print(not(y > 10 and y % 5 == 0))# False because the second condition is false 
